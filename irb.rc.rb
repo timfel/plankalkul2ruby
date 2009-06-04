b7c746e5b72f55705e5d52405533063ce0b8127c
Tim Felgentreff <tim@medion.(none)>
2009-06-05 00:53:30 +0200

    remove sexp from master

--- irb.rc.rb
+++ irb.rc.rb
@@ -1,4 +1,4 @@
-PROGRAM = "P815 (V0[:8.0])=>(R0[:8.0])
+PKPROGRAM = "P815 (V0[:8.0])=>(R0[:8.0])
 FIN
 V0[:8.0]=2->4+1=>Z0[:2.2.0]
 w1(V0[:0]-1)[1+2]
@@ -7,10 +7,9 @@ w1(5)[5+6]
 w1[7+8]
 END"
 
-require 'sexp_stuff'
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), "lib")
 require 'pk2000core'
 PARSER  = Pk2000Parser.new
 TESTPROG = "15=>Z0[:4.0]
 1=>Z1[:4.0]"
-TREE = PARSER.parse(PROGRAM)
+TREE = PARSER.parse(PKPROGRAM)
