d43bc9751178791ec79302e15439d1eb31839e2d
Tim Felgentreff <tim@medion.(none)>
2009-06-07 02:49:22 +0200

    switch to new runtime

--- irb.rc.rb
+++ irb.rc.rb
@@ -1,10 +1,12 @@
-PKPROGRAM = "P815(V0[:8.0],V1[:2.0])=>(R0[:8.0])
-FIN
+PKPROGRAM = "P815(V0[:32.0],V1[:2.0])=>(R0[:8.0])
+V0[:32.0]+5=>R0[:8.0]
 END"
 
 require 'sexp_stuff'
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), "lib")
 require 'pk2000core'
+require 'pk2000runtime'
+include Plankalkuel
 PARSER  = Pk2000Parser.new
 TESTPROG = "15=>Z0[:4.0]
 1=>Z1[:4.0]"
