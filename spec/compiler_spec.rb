ba42a76e70e0eb13c790861aeb0394e56626f17c
Tim Felgentreff <tim@medion.(none)>
2009-06-07 18:47:23 +0200

    split spec

---
+++ spec/compiler_spec.rb
@@ -0,0 +1,125 @@
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), "..", "lib")
+require 'pk2000core'
+require 'pk2000runtime'
+require 'ruby2ruby'
+
+include Plankalkuel
+
+describe "Compiler" do
+   before do
+      @parser = Pk2000Parser.new
+      PKVariable.resetVariableSpace
+   end
+
+   def r2r(string)
+      sexp = @parser.parse(string).toRuby
+      sexp.should.class == Sexp
+      code = Ruby2Ruby.new.process(sexp)
+      code.should.class == String 
+      sexp = @parser.parse(string).toRuby
+      [code, sexp]
+   end
+
+   def compile(string, equal=nil)
+      unless equal
+	 return r2r(string)[1]
+      end
+      code = r2r(string)[0]
+      eval(code).should.class == equal unless equal.nil?
+      eval(code)
+   end
+
+   it "compiles numbers" do
+      compile("6", Fixnum).should == 6
+      compile("0", Fixnum).should == 0
+      compile("10", Fixnum).should == 10
+   end
+
+   it "compiles prefixes" do
+      compile("!1", FalseClass)
+      compile("-1", Fixnum).should == -1
+   end
+
+   it "compiles generic variables" do
+      compile("i1")[1][2].should == :i1
+      compile("i")[1][2].should == :i
+   end
+
+   it "compiles logic constants" do
+      pending
+      compile("+", Array)
+      compile("-", Array)
+      compile("--+", Array)
+   end
+
+   it "compiles variables" do
+      compile("Z0[:0]", PKVariable)
+      compile("Z1[:(8.0,2.0)]", PKVariable)
+      compile("Z2[:8.0]", PKVariable)
+      compile("Z2[0:0]", PKVariable)
+      compile("(Z2[0:0], Z3[:2.0])", PKTuple)
+   end
+
+   it "compiles operations" do
+      compile("1+0", Fixnum).should == 1
+      compile("5-5", Fixnum).should == 0
+      compile("5/5", Fixnum).should == 1
+      compile("5*5", Fixnum).should == 25
+      compile("5|5", Fixnum)
+      compile("5~5", Fixnum)
+   end
+
+   it "compiles conditions" do
+      compile("1=1", TrueClass)
+      compile("2<1", FalseClass)
+      compile("2>1", TrueClass)
+      compile("(Z0[:2.0],Z1[:3.0])=(1,2)", FalseClass)
+   end
+
+   it "compiles the count macro" do
+      compile("N(Z0[:2.0])", Fixnum).should == 2
+      compile("N(Z1[:(2.0,3.0)])", Fixnum).should == 2
+   end
+
+   it "compiles function-calls" do
+      pending
+      compile("R0815[1:0](1)")[1][0].should == :call
+      compile("R0815[1:0](1)")[1][1].should == :PK0815
+   end
+
+   it "compiles FIN statements" do
+      compile("FIN")[1][0].should == :break
+      compile("FIN2")[1][0].should == :break
+   end
+
+   it "compiles blocks" do
+      compile("[5+2]", Fixnum).should == 7
+      compile("[5+2\n8+2]", Fixnum).should == 10
+   end
+
+   it "compiles ifThens" do
+      compile("1->5", Fixnum)
+      compile("1->5")[1][0].should == :if
+      compile("1->[1+2\n5]", Fixnum)
+      compile("1->[1+2\n5]")[1][0].should == :if
+   end
+
+   it "compiles loops" do
+      pending
+      compile("w[1->3+4]")
+      compile("w1(5)[5>4->5+1]")
+      compile("w1[5>4->5+1]")
+   end
+
+   it "compiles assignments" do
+      compile("2=>Z0[:2.0]", PKVariable).to_i.should == 2
+   end
+
+   it "compiles functions" do
+      compile("P815(V0[:8.0])=>(R0[:8.0])\nFIN\nEND")[0].should == :defs
+      compile("P815(V0[:8.0])=>(R0[:8.0])\nFIN\nEND")[1][1].should == :Plankalkuel
+      compile("P815(V0[:8.0])=>(R0[:8.0])\nFIN\nEND")[2].should == :p815
+      compile("P815(V0[:8.0])=>(R0[:8.0])\nFIN\nEND")[3][1].should == :v0
+      compile("P815(V0[:8.0])=>(R0[:8.0])\nFIN\nEND")[4][0] == :scope
+   end
+end
---
+++ spec/runtime_spec.rb
@@ -0,0 +1,66 @@
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), "..", "lib")
+require 'pk2000runtime'
+include Plankalkuel
+
+describe PKVariable do 
+   before do 
+      PKVariable.resetVariableSpace
+   end
+
+   it "is only readable if it is input value" do
+      PKVariable.instance(["V0", "", "8.0"]).readable?.should_not.nil?
+      PKVariable.instance(["V0", "", "8.0"]).writeable?.should.nil?
+   end
+
+   it "is only writeable if it is return value" do
+      PKVariable.instance(["R0", "", "8.0"]).writeable?.should_not.nil?
+      PKVariable.instance(["R0", "", "8.0"]).readable?.should.nil?
+   end
+
+   it "is R/W if it is local variable" do
+      PKVariable.instance(["Z0", "", "8.0"]).readable?.should_not.nil?
+      PKVariable.instance(["Z0", "", "8.0"]).writeable?.should_not.nil?
+   end
+
+   it "is combinable only with same-dimension variables" do
+      v8_1 = PKVariable.instance(["Z0", "", "8.0"])
+      v8_2 = PKVariable.instance(["Z1", "", "8.0"])
+      v4_1 = PKVariable.instance(["Z2", "", "4.0"])
+      (v8_1 + v8_2).should == 0
+      proc { (v8_1 + v4_1) }.should raise_error
+   end
+
+   it "is always combinable with constants" do
+      v4_1 = PKVariable.instance(["Z2", "", "4.0"])
+      (v4_1 + 2).should == 2
+      v4_1 <= v4_1 + 2
+      (v4_1 - 1).should == 1
+      (v4_1 * 2).should == 4
+      (v4_1 / 2).should == 1
+      # einmal für negative zuweisung
+      v4_1 <= -1
+      (v4_1 == -1).should == true
+      # einmal für zuweisung im wertebereich
+      v4_1 <= 4
+      (v4_1 == 4).should == true
+      v4_1.to_i.should == 4
+      # einmal für zuweisung oberhalb des wertebereiches
+      v4_1 <= 128
+      (v4_1 == 128).should == true
+   end
+
+   it "selects always the same variable with the same selector" do
+      z1 = PKVariable.instance(["Z0", "", "8.0"])
+      z2 = PKVariable.instance(["z0", "", "8.0"])
+      (z1.equal? z2).should == true
+      z2 = PKVariable.instance(["Z0", "0", "0"])
+      (z1.equal? z2).should == true
+   end
+
+   it "can be selected only type-safely" do
+      v = PKVariable.instance(["Z0", "", "4.2.0"])
+      PKVariable.instance(["Z0", "1", "2.0"]).should_not.nil?
+      proc { PKVariable.instance(["Z0", "1", "4.2.0"]) }.should raise_error(ArgumentError)
+   end
+end
+
