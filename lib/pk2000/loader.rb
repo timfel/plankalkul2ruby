b3035876b0818b4cbe45a85c0a289b273b93b6fa
Tim Felgentreff <tim@medion.(none)>
2009-06-08 18:50:50 +0200

    fix that, now works

--- lib/pk2000/loader.rb
+++ lib/pk2000/loader.rb
@@ -1,9 +1,19 @@
-require 'polyglot'
-
 class PlankalkuelLoader
    def self.load(filename, options = nil, &block)
       File.open(filename) do |file|
+	 parser = Pk2000Parser.new
+	 #puts file.read
+	 pkCode = file.read.
+	    gsub(" ", "").gsub("\n]", "]").
+	    gsub("[\n", "[").gsub(/^\n$/, "").
+	    gsub("END\n", "END")
+	 tree = parser.parse(pkCode)
+	 rbCode = Ruby2Ruby.new.process(tree.toRuby)
+	 out = "require 'pk2000'\n"
+	 out << "include Plankalkuel\n"
+	 out << rbCode
+	 eval(out)
       end
    end
 end
-
+Polyglot.register("pk", PlankalkuelLoader)
