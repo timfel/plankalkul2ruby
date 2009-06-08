cb2f5cc4ae70cfdcaf0ed72a2f8d9642214b6546
Tim Felgentreff <tim@medion.(none)>
2009-06-08 20:25:11 +0200

    add last test for now

--- examples_pk/testrequire.rb
+++ examples_pk/testrequire.rb
@@ -3,12 +3,16 @@ $LOAD_PATH.unshift Pathname.new(__FILE__).dirname.join("../lib").expand_path.to_
 require 'pk2000'
 require 'invert'
 require 'timesplus5'
+require 'factorial'
 
-puts "\nInvertiere 0: "+Plankalkuel.p4(0).to_s
-puts "Invertiere 1: "+Plankalkuel.p4(1).to_s
+puts "\nInvertiere 0: "+Plankalkuel.p4(0).to_i.to_s
+puts "Invertiere 1: "+Plankalkuel.p4(1).to_i.to_s
 
-puts "\nFaktorisiere 4: "+Plankalkuel.p1(4).to_i.to_s
-puts "Faktorisiere 2: "+Plankalkuel.p1(2).to_i.to_s
+puts "\nAddiere 4x 5: "+Plankalkuel.p1(4).to_i.to_s
+puts "Addiere 2x 5: "+Plankalkuel.p1(2).to_i.to_s
+
+puts "\nFakultat 4: "+Plankalkuel.p5(4).to_i.to_s
+puts "Fakultat 2: "+Plankalkuel.p5(2).to_i.to_s
 
 puts "\n...done through the power of the PK2000"
 
