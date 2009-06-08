2e50b8ec2781952b5d8be5d69697578cf5b0962a
Tim Felgentreff <tim@medion.(none)>
2009-06-08 20:11:44 +0200

    return only one value and also try to include that

--- examples_pk/testrequire.rb
+++ examples_pk/testrequire.rb
@@ -2,8 +2,13 @@ require "pathname"
 $LOAD_PATH.unshift Pathname.new(__FILE__).dirname.join("../lib").expand_path.to_s
 require 'pk2000'
 require 'invert'
+require 'timesplus5'
 
-puts "\nInvertiere 0: "+Plankalkuel.p4(0).to_i.to_s
-puts "Invertiere 1: "+Plankalkuel.p4(1).to_i.to_s
+puts "\nInvertiere 0: "+Plankalkuel.p4(0).to_s
+puts "Invertiere 1: "+Plankalkuel.p4(1).to_s
+
+puts "\nFaktorisiere 4: "+Plankalkuel.p1(4).to_i.to_s
+puts "Faktorisiere 2: "+Plankalkuel.p1(2).to_i.to_s
+
 puts "\n...done through the power of the PK2000"
 
