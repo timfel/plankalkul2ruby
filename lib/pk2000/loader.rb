a7eacbad5f89abf40e049887e873d21a586f91cd
Tim Felgentreff <tim@medion.(none)>
2009-06-12 22:56:07 +0200

    remove empty lines on parsing

--- lib/pk2000/loader.rb
+++ lib/pk2000/loader.rb
@@ -2,10 +2,10 @@ class PlankalkuelLoader
    def self.load(filename, options = nil, &block)
       File.open(filename) do |file|
 	 parser = Pk2000Parser.new
-	 #puts file.read
 	 pkCode = file.read.
 	    gsub(" ", "").gsub("\n]", "]").
 	    gsub("[\n", "[").gsub(/^\n$/, "").
+	    gsub("\n\n", "\n").
 	    gsub("END\n", "END")
 	 tree = parser.parse(pkCode)
 	 rbCode = Ruby2Ruby.new.process(tree.toRuby)
